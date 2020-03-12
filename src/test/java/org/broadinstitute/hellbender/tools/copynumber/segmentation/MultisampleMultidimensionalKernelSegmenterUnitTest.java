b0239f1654466c78699e9642a2695f05fcac9685
Samuel Lee <lee.samuel.k@gmail.com>
2020-03-12 13:49:48 -0400

    comment out local tests

---
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/segmentation/MultisampleMultidimensionalKernelSegmenterUnitTest.java
@@ -0,0 +1,9 @@
+package org.broadinstitute.hellbender.tools.copynumber.segmentation;
+
+import org.broadinstitute.hellbender.GATKBaseTest;
+
+import static org.testng.Assert.*;
+
+public final class MultisampleMultidimensionalKernelSegmenterUnitTest extends GATKBaseTest {
+
+}
